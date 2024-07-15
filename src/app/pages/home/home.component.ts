d3e7f1a3620bbdd8edf5a2cbbd7a74ff876f9d0d
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-15 16:23:48 -0500

    chore: Update home component with task list and checkboxes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  tasks = signal([
+    'Instalar Angular CLI',
+    'Crear nuevo proyecto',
+    'Crear componentes',
+  ]);
 }
