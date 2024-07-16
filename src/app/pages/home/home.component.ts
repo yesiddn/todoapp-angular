97de5df9d9b1c7e1343e6a476834af57ec19dd61
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-16 11:42:46 -0500

    chore: Add FormControl to create a new task in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common'
-
+import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -33,14 +33,35 @@ export class HomeComponent {
     // 'Crear componentes',
   ]);
 
+  newTaskCtrl = new FormControl(
+    '',
+    // se puede pasar un array de validadores al FormControl
+    {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        // hay muchos validadores que se pueden usar, como Validators.minLength(3), Validators.maxLength(10), Validators.pattern(/^[a-zA-Z]+$/), etc
+      ],
+    }
+  ); // se inicializa el FormControl con un valor por defecto, en este caso un string vacio
+
+  // antes del uso de FormControl con el signal binding
+  // changeHandler(event: Event) {
+  //   const input = event.target as HTMLInputElement;
+  //   const newTask = input.value;
+
+  //   // se separan responsabilidades, por lo que esta linea de código se traslada a la función addTask
+  //   // this.tasks.update((tasks) => [...tasks, newTask]); // de esta forma se actualiza el valor de una señal -> update nos da como argumento el valor actual de la señal para que podamos modificarlo -> viene del patron No Mutar, Solo Crear Nuevos Estados
+
+  //   this.addTask(newTask);
+  // }
+
   changeHandler(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-
-    // se separan responsabilidades, por lo que esta linea de código se traslada a la función addTask
-    // this.tasks.update((tasks) => [...tasks, newTask]); // de esta forma se actualiza el valor de una señal -> update nos da como argumento el valor actual de la señal para que podamos modificarlo -> viene del patron No Mutar, Solo Crear Nuevos Estados
-
-    this.addTask(newTask);
+    if (this.newTaskCtrl.valid && this.newTaskCtrl.value.trim() !== '') {
+      const value = this.newTaskCtrl.value;
+      this.addTask(value.trim());
+      this.newTaskCtrl.setValue('');
+    }
   }
 
   addTask(title: string) {
@@ -53,7 +74,9 @@ export class HomeComponent {
   }
 
   deleteTask(index: number) {
-    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
   }
 
   updateTask(index: number) {
