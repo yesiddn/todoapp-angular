7d8793a404427988212775d1c350a2dca6997f61
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-16 10:08:06 -0500

    feat: Add Task interface and update task status functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common'
 
+import { Task } from '../../models/task.model';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,19 +11,57 @@ import { CommonModule } from '@angular/common'
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear nuevo proyecto',
-    'Crear componentes',
+  // con la interface Task hacemos que tasks sea un array de objetos con las propiedades id, title y completed. En caso de que no se cumpla la interface, TypeScript nos mostrará un error
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false,
+    },
+    {
+      id: Date.now() + 1,
+      title: 'Crear nuevo proyecto',
+      completed: false,
+    },
+    {
+      id: Date.now() + 2,
+      title: 'Crear componentes',
+      completed: false,
+    },
+    // 'Instalar Angular CLI',
+    // 'Crear nuevo proyecto',
+    // 'Crear componentes',
   ]);
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask]); // de esta forma se actualiza el valor de una señal -> update nos da como argumento el valor actual de la señal para que podamos modificarlo -> viene del patron No Mutar, Solo Crear Nuevos Estados
+
+    // se separan responsabilidades, por lo que esta linea de código se traslada a la función addTask
+    // this.tasks.update((tasks) => [...tasks, newTask]); // de esta forma se actualiza el valor de una señal -> update nos da como argumento el valor actual de la señal para que podamos modificarlo -> viene del patron No Mutar, Solo Crear Nuevos Estados
+
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    // de esta forma se puede cambiar el valor de una propiedad de un objeto dentro de un array sin mutar el estado
+    this.tasks.update((tasks) =>
+      tasks.map((task, position) =>
+        position === index ? { ...task, completed: !task.completed } : task
+      )
+    );
   }
 }
